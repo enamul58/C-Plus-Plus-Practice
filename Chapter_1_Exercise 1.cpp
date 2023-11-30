997cfa0a2740f9c6a99976eabbcaeaa40a58187d
“enamul58” <enamullimon@gmail.com>
2023-11-30 23:22:31 +0600

    cout use print some text in output console

---
+++ Chapter_1_Exercise 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout <<"Oh what\n";
+    cout << "a happy day!\n";
+    cout << "Oh yes,\n";
+    cout << "what a happy day!";
+    return 0;
+}
